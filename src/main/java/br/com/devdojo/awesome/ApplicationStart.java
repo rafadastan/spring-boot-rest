4ab3c74af34d8b3115b0479071c726acf40cd7f3
rafadastan <rafadastan02@live.com>
2019-08-19 23:29:30 -0300

    Alterações dos arquivos

--- src/main/java/br/com/devdojo/awesome/ApplicationStart.java
+++ src/main/java/br/com/devdojo/awesome/ApplicationStart.java
@@ -1,11 +1,12 @@
-package br.com.devdojo.awesome.start;
+package br.com.devdojo.awesome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "br.com.devdojo.awesome.endpoint")
+@SpringBootApplication
 public class ApplicationStart {
     public static void main(String[] args){
         SpringApplication.run(ApplicationStart.class,args);
